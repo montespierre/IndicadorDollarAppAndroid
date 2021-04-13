8e2aee6965c6fa7e6fdf62135c796e08050521ab
unknown <montespierre@gmail.com>
2021-04-12 21:58:34 -0500

    retrofit

--- app/src/main/java/com/weimont/indicadordollarappandroid/activities/MainActivity.java
+++ app/src/main/java/com/weimont/indicadordollarappandroid/activities/MainActivity.java
@@ -1,16 +1,35 @@
 package com.weimont.indicadordollarappandroid.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.weimont.indicadordollarappandroid.R;
+import com.weimont.indicadordollarappandroid.viewmodel.DolarIndicadorViewModel;
 
 public class MainActivity extends AppCompatActivity {
+    // Dollar Indicador Api
+    // https://mindicador.cl/api/dolar
+
+    private DolarIndicadorViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(DolarIndicadorViewModel.class);
+        getDolarIndicador();
+    }
+
+    private void getDolarIndicador() {
+        viewModel.getDolarIndicador().observe(this, dolarIndicadorResponse ->
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Total" + dolarIndicadorResponse.getIndicadors(),
+                                Toast.LENGTH_SHORT
+                        ).show()
+                );
     }
 }
--- app/src/main/java/com/weimont/indicadordollarappandroid/repositories/DolarIndicadorRepository.java
+++ app/src/main/java/com/weimont/indicadordollarappandroid/repositories/DolarIndicadorRepository.java
@@ -34,6 +34,6 @@ public class DolarIndicadorRepository {
             }
         });
 
-        return null;
+        return data;
     }
 }
